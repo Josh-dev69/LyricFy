eb7198f82923d700ed9cfb13954571cdc4a268f3
Josh-dev69 <ebubejoshua69@gmail.com>
2024-07-08 14:47:31 +0100

    Created the NavBar for the Header files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Search from "./pages/Search";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,14 @@
+import { TextInput } from 'flowbite-react'
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+        <TextInput>
+          
+        </TextInput>
+    </div>
+  )
+}
+
+export default Search
