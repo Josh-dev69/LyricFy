a9c85dac6fbfbe3b6baa01d8d7298cf4c0a23026
Josh-dev69 <ebubejoshua69@gmail.com>
2024-07-08 11:17:35 +0100

    Setting the Environment

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
